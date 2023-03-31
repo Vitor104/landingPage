717b2e68429e4e74803c8aaf368f40eda82e9bfd
João Vitor da Silva Oliveira <jv7554817@gmail.com>
2023-03-31 10:49:55 -0300

    fazendo testes com js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@ let btn = document.querySelector("button");
 let param = document.querySelector(".param");
 
 btn.addEventListener("click", trocaFoto);
+imagem.addEventListener("mouseout", inicio);
+
+
 
 
 function trocaFoto () {
-    imagem.src = "img/coffee.jpg"
-    param.innerHTML = "sou um paragraf"
+    imagem.src = "img/mulhercafe.jpg"
+    param.innerHTML = "- Sed mollitia excepturi incidunt quia, modi molestias voluptas ut! Officia officiis, cum dolorum ad quo, voluptate vero."
 }
+
+function inicio() {
+    imagem.src = "img/imagemcafe.jpg"
+    param.innerHTML = "- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed mollitia excepturi incidunt quia, modi molestias voluptas ut! Officia officiis, cum dolorum ad quo, voluptate vero, quasi veniam dignissimos reiciendis suscipit."
+}
+
+
+
+
+
