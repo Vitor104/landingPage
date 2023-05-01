6b107821014c015e15b14498b0429de21b54de47
João Vitor da Silva Oliveira <jv7554817@gmail.com>
2023-05-01 14:59:47 -0300

    a base está pronta

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-function myFunction() {
-    let tela = document.querySelector(".body").classList.toggle("mudar");
-
-    let palavra = document.querySelector(".mudanca").classList.toggle("mudanca-dois")
-}
 
 function myPorta() {
-    let porta = document.querySelector(".porta").classList.toggle("aparecer");
+    let porta = document.querySelector("header").style.background = "rgb(3, 3, 43)";
+    let sol = document.querySelector("#sol").style.display = "none";
+    let lua = document.querySelector("#lua").style.display = "block";
+    let corpo = document.querySelector("body").style.background = "rgb(3, 3, 43)";
+
+    let nuvemum = document.querySelector("#nuvem-um").style.display = "none";
+    let nuvemdois = document.querySelector("#nuvem-dois").style.display = "none";
+    let nuvemtres = document.querySelector("#nuvem-tres").style.display = "none";
+    let nuvemquatro = document.querySelector("#nuvem-quatro").style.display = "none";
+    let nuvemcinco = document.querySelector("#nuvem-cinco").style.display = "none";
+
+    let estrelaum = document.querySelector("#estrela-um").style.display = "block";
+    let estreladois = document.querySelector("#estrela-dois").style.display = "block";
+    let estrelatres = document.querySelector("#estrela-tres").style.display = "block";
+    let estrelaquatro = document.querySelector("#estrela-quatro").style.display = "block";
+    let estrelacinco = document.querySelector("#estrela-cinco").style.display = "block";
+}
+
+function myFunction() {
+    let sol = document.querySelector("#sol").style.display = "block";
+    let lua = document.querySelector("#lua").style.display = "none";
+    let porta = document.querySelector("header").style.background = "skyblue";
+    let corpo = document.querySelector("body").style.background = "skyblue";
+
+    let nuvemum = document.querySelector("#nuvem-um").style.display = "block";
+    let nuvemdois = document.querySelector("#nuvem-dois").style.display = "block";
+    let nuvemtres = document.querySelector("#nuvem-tres").style.display = "block";
+    let nuvemquatro = document.querySelector("#nuvem-quatro").style.display = "block";
+    let nuvemcinco = document.querySelector("#nuvem-cinco").style.display = "block";
+
+    let estrelaum = document.querySelector("#estrela-um").style.display = "none";
+    let estreladois = document.querySelector("#estrela-dois").style.display = "none";
+    let estrelatres = document.querySelector("#estrela-tres").style.display = "none";
+    let estrelaquatro = document.querySelector("#estrela-quatro").style.display = "none";
+    let estrelacinco = document.querySelector("#estrela-cinco").style.display = "none";
 }
